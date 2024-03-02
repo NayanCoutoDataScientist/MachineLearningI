9ec64abb3de3ea43483d006e43b3be84b2414f84
Nayan Couto <pereira.nayan@gmail.com>
2024-03-02 00:27:18 -0300

    Unidade_IV_4

---
+++ Unidade_IV/Aula_4_Aprendizagem_supervisionada.py
@@ -0,0 +1,40 @@
+# Como ocorre a aprendizagem supervisionada e a importância uma boa preparação dos dados
+"""
+> Em machine learning, o método de aprendizado depende da estrutura dos dados usados na entrada, além da estratédia
+usada para o aprendizado.
+
+Tipos de aprendizagem de máquina:
+> Aprendizado supervisionado;
+> Aprendizado não supervisionado;
+> Aprendizado semi-supervisionado;
+> Aprendizado por reforço;
+
+Problemas:
+> Quantidade insuficiente de dados de treinamento;
+> Dados de treinamento não são representativos;
+> Baixa qualidade nos dados;
+> Características irrelevantes;
+> Sobreajuste nos dados de treinamento;
+
+Pré-processamento de dados:
+> Limpeza
+> Integração
+> Redução
+> Transformação
+> Discretização
+"""
+
+# Multilayer Perceptron
+"""
+As redes neurais Perceptron de múltiplas camadas podem ser abstraídas em quatro principais etapas; são elas:
+(1) inicialização, em que valores aleatórios são atribuídos aos pesos e limites;
+(2) ativação, em que é realizado o cálculo dos valores dos neurônios das camadas ocultas e dos valores da camada de saída;
+(3) treinamento dos pesos, em que é calculada a taxa de erro e aplicada a correção e a atualização dos pesos;
+(4) iteração, em que o processo é repetido até que o critério de erro definido na inicialização seja cumprido;
+"""
+
+# Introdução ao Deep Learning
+"""
+> Deep learning (aprendizado profundo) é uma das técnicas de aprendizagem de máquina (machine learning) que compõem o
+campo da inteligência artificial.
+"""
